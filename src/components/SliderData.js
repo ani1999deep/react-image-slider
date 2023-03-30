cdd71d99e64dcf64beb4e5bec9cc5d98ac7978ba
ani1999deep <anideepbhowmick1@gmail.com>
2023-03-30 16:44:15 +0530

    first commit

---
+++ src/components/SliderData.js
@@ -0,0 +1,19 @@
+export const SliderData=[
+
+    {
+        image:'https://images.unsplash.com/photo-1626198226928-617fc6c6203e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
+    },
+
+    {
+        image:'https://images.unsplash.com/photo-1559406041-c7d2b2e98690?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDh8fEtvbGthdGF8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60'
+    },
+    {
+        image:'https://images.unsplash.com/photo-1600678977783-0f1038813147?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=427&q=80'
+    },
+    {
+        image:'https://images.unsplash.com/photo-1630202328184-900c1190b790?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80'
+    },
+    {
+        image:'https://images.unsplash.com/photo-1625728684480-b2dff91cc5a9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80'
+    }
+]
